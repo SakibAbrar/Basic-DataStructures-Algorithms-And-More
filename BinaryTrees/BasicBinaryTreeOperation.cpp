a085b72ec8d4a7c6bb74b337429e3243900806e1
SakibAbrar <sakib.cghs@gmail.com>
2022-06-14 03:05:15 +0600

    Add Iterative Preorder Traversal and write test for it

--- BinaryTrees/BasicBinaryTreeOperation.cpp
+++ BinaryTrees/BasicBinaryTreeOperation.cpp
@@ -42,6 +42,32 @@ class Solution {
         }
         return visitOrder;
     }
+
+    // Traversal Order : root, left, right
+    vector<int> preorderIterativeTraversal(TreeNode *root) {
+        vector<int> visitOrder;  // this will keep track of the visited nodes sequentially
+        stack<TreeNode *> stk;   // a stack that will keep the yet to process/visit nodes
+        TreeNode *curr = root;   // this will point to the current node
+
+        while (!stk.empty() || curr != nullptr) {
+            // when we visit any node the first time, we process it,
+            // push the node into the stack (to process the right subtree of that node later), and go to the left child.
+            if (curr != nullptr) {
+                visitOrder.push_back(curr->val);
+                stk.push(curr);
+                curr = curr->left;
+            }
+
+            // If there’s no left child, we grab one node from the top of the stack and go to the right child of that node.
+            // Now we continue the same process for the subtree rooted at the right child of the popped node.
+            else {
+                curr = stk.top();
+                stk.pop();
+                curr = curr->right;
+            }
+        }
+        return visitOrder;
+    }
 };
 
 class Test {
@@ -68,11 +94,38 @@ class Test {
         for (int num : res) {
             cout << num << " ";
         }
+        cout << endl;
+    }
+
+    void testPreorderIterativeTraversal() {
+        /*
+        Constructed binary tree is
+                  1
+                /   \
+              2      3
+            /  \
+          4     5
+
+        */
+        TreeNode *treeRoot = new TreeNode(1);
+        treeRoot->left = new TreeNode(2);
+        treeRoot->right = new TreeNode(3);
+        treeRoot->left->left = new TreeNode(4);
+        treeRoot->left->right = new TreeNode(5);
+
+        Solution sln;
+        vector<int> res = sln.preorderIterativeTraversal(treeRoot);
+
+        for (int num : res) {
+            cout << num << " ";
+        }
+        cout << endl;
     }
 };
 
 int main() {
     Test test;
-    test.testInorderIterativeTraversal();
+    // test.testInorderIterativeTraversal();
+    test.testPreorderIterativeTraversal();
     return 0;
 }
